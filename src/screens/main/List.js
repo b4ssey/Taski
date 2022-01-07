99b09a360a90b66ee955d339d1a6017fc7eb8011
Ekemini <42721358+b4ssey@users.noreply.github.com>
2022-01-07 14:40:02 +0000

    added drawerlayout without functionality

--- src/screens/main/List.js
+++ src/screens/main/List.js
@@ -1,14 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { View, StyleSheet, Platform } from "react-native";
-import { FlatList } from "react-native-gesture-handler";
-import { Appbar, Searchbar, Text } from "react-native-paper";
+import { View, StyleSheet, Platform, Dimensions } from "react-native";
+import { FlatList, DrawerLayout } from "react-native-gesture-handler";
+import { Appbar, Searchbar, Drawer } from "react-native-paper";
 import Taski from "../../../assets/taski.svg";
 import TaskiEmpty from "../../../assets/taskiEmpty.svg";
 import Todo from "../../components/list/Todo";
 import AppSafeAreaView from "../../components/reusables/AppSafeAreaView";
 
-const emptydata = [];
 const dummydata = [
   {
     id: 1,
@@ -48,56 +47,124 @@ const dummydata = [
   },
 ];
 
+const handleDrawerSlide = (status) => {
+  // outputs a value between 0 and 1
+  // console.log("status", status);
+};
+
 function List(props) {
   const [searchQuery, setSearchQuery] = useState("");
+  const [active, setActive] = React.useState("");
+  let drawer = null;
+
+  const renderDrawer = () => {
+    return (
+      <>
+        <View style={{ flex: 1, justifyContent: "center" }}>
+          <Drawer.Section title="Others....">
+            <Drawer.Item
+              label="Dark Mode"
+              active={active === "first"}
+              onPress={() => setActive("first")}
+            />
+            <Drawer.Item
+              label="Send Feedback"
+              active={active === "second"}
+              onPress={() => setActive("second")}
+            />
+            <Drawer.Item
+              label="Buy me a Coffee"
+              active={active === "third"}
+              onPress={() => setActive("third")}
+            />
+          </Drawer.Section>
+          <Drawer.Section title="About....">
+            <Drawer.Item
+              label="You"
+              active={active === "F"}
+              onPress={() => setActive("F")}
+            />
+            <Drawer.Item
+              label="Me"
+              active={active === "G"}
+              onPress={() => setActive("G")}
+            />
+          </Drawer.Section>
+        </View>
+      </>
+    );
+  };
 
   const onChangeSearch = (query) => setSearchQuery(query);
   return (
     <>
       <StatusBar hidden />
-      <Appbar style={styles.bar}>
-        <Appbar.Action icon="menu" onPress={() => {}} />
-        {Platform.OS == "android" && <Taski width={66} />}
-        <Appbar.Action icon="dots-vertical" onPress={() => {}} />
-      </Appbar>
-      <AppSafeAreaView SAVstyle={{ paddingTop: "2.5%" }}>
-        <FlatList
-          data={dummydata}
-          keyExtractor={(item) => item.id.toString()}
-          ListEmptyComponent={() => {
-            return (
-              <View
-                style={{
-                  alignItems: "center",
-                  marginTop: "33.333%",
-                }}
-              >
-                <TaskiEmpty />
-              </View>
-            );
-          }}
-          showsVerticalScrollIndicator={false}
-          ListHeaderComponent={() => {
-            return (
-              <Searchbar
-                placeholder="Search task here"
-                onChangeText={onChangeSearch}
-                value={searchQuery}
-              />
-            );
-          }}
-          renderItem={({ item }) => {
-            return (
-              <Todo
-                title={item.title}
-                timeDate={item.timeDate}
-                notes={item.notes}
-                tag={item.tag}
-              />
-            );
-          }}
-        />
-      </AppSafeAreaView>
+      <DrawerLayout
+        ref={(refDrawer) => (drawer = refDrawer)}
+        drawerWidth={Dimensions.get("window").width * 0.6}
+        drawerPosition={DrawerLayout.positions.Left}
+        drawerType="front"
+        drawerBackgroundColor="#ddd"
+        renderNavigationView={() => renderDrawer()}
+        onDrawerSlide={() => handleDrawerSlide()}
+      >
+        <Appbar style={styles.bar}>
+          <Appbar.Action icon="menu" onPress={() => drawer.openDrawer()} />
+          {Platform.OS == "android" && <Taski width={66} />}
+          <Appbar.Action icon="dots-vertical" onPress={() => {}} />
+        </Appbar>
+        <AppSafeAreaView SAVstyle={{ paddingTop: "2.5%" }}>
+          <FlatList
+            data={dummydata}
+            keyExtractor={(item) => item.id.toString()}
+            ListEmptyComponent={() => {
+              return (
+                <View
+                  style={{
+                    alignItems: "center",
+                    marginTop: "33.333%",
+                  }}
+                >
+                  <TaskiEmpty />
+                </View>
+              );
+            }}
+            showsVerticalScrollIndicator={false}
+            ListHeaderComponent={() => {
+              return (
+                <Searchbar
+                  placeholder="Search task here"
+                  onChangeText={onChangeSearch}
+                  value={searchQuery}
+                />
+              );
+            }}
+            renderItem={({ item }) => {
+              return (
+                <Todo
+                  title={item.title}
+                  timeDate={item.timeDate}
+                  notes={item.notes}
+                  tag={item.tag}
+                />
+              );
+            }}
+          />
+        </AppSafeAreaView>
+      </DrawerLayout>
+
+      {/* <DrawerLayout
+        drawerWidth={200}
+        drawerPosition={DrawerLayout.positions.Right}
+        drawerType="front"
+        drawerBackgroundColor="#ddd"
+        renderNavigationView={renderDrawer}
+        onDrawerSlide={handleDrawerSlide}
+      >
+        <View>
+          <Text>Hello, it's me</Text>
+        </View>
+      </DrawerLayout> */}
     </>
   );
 }
